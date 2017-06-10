d79e658e19abba3631e8c8f6d0d40dcb06441349
Sriram Thiagarajan <sri.sjc@gmail.com>
2017-06-10 15:39:53 +0530

    login page style

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,78 @@
 import * as React from 'react';
 
-export default function Login(props: React.Props<any>) {
-    return (
-        <div>Login</div>
-    )
+const classNames = require('classnames');
+
+export default class Login extends React.PureComponent<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            submittig: false,
+            error: null
+        }
+
+        this.submit = this.submit.bind(this);
+    }
+
+    submit(e: React.SyntheticEvent<any>) {
+        e.preventDefault();
+        const { username, password } = this.state;
+        if(!(username && password)) {
+            this.setState({
+                error: 'Invalid username or password'
+            });
+        } else {
+            this.setState({
+                submitting: true
+            });
+            // do submit
+        }
+    }
+
+    render() {
+        const submitBtnClass = classNames({
+            'button': true,
+            'is-primary': true,
+            'is-loading': this.state.submitting
+        });
+        return (
+            <div className='hero is-fullheight'>
+                <div className='hero-body'>
+                    <div className='container has-text-centered'>
+                        <div className='container'>
+                            <div className='columns'>
+                                <div className='column is-half is-offset-one-quarter'>
+                                    <div className='box'>
+                                        <h2 className='title is-2 has-text-centered'>Login</h2>
+                                        <form className='form' onSubmit={this.submit}>
+                                            <div className='field'>
+                                                <p className='control'>
+                                                    <input className='input' name='username' placeholder='Username'
+                                                        value={this.state.username} autoFocus={true}
+                                                        onChange={(e) => this.setState({username: e.target.value})} />
+                                                </p>
+                                            </div>
+                                            <div className='field'>
+                                                <p className='control'>
+                                                    <input className='input' type='password' name='password'
+                                                        placeholder='Password' value={this.state.password}
+                                                        onChange={(e) => this.setState({password: e.target.value})} />
+                                                </p>
+                                            </div>
+                                            <div className='field'>
+                                                <p className='control'>
+                                                    <button className={submitBtnClass}>Login</button>
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
