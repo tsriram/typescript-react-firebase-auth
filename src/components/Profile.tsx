d4ac1eb66c918f0463bfc05e7f1c0f372fff6b74
Sriram Thiagarajan <sri.sjc@gmail.com>
2017-06-11 16:00:28 +0530

    profile ui change

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -8,6 +8,26 @@ import { logout } from '../util/auth';
 interface ProfileProps extends ExtendedRouteProps {
     user?: User;
 }
+const avatarStyle = {
+    borderRadius: '50%',
+    height: 75,
+    width: 75
+}
+
+const getAvatar = (user: User) => {
+    if(user.photoURL) {
+        return (
+            <img src={user.photoURL} style={avatarStyle} />
+        )
+    } else if(user.providerData.length) {
+        const photoURL = user.providerData[0].photoURL;
+        return (
+            <img src={photoURL} style={avatarStyle} />
+        )
+    } else {
+        <Gravatar email={user.email} style={avatarStyle} />
+    }
+}
 
 export default function Profile(props: ProfileProps) {
     const { user } = props;
@@ -16,7 +36,7 @@ export default function Profile(props: ProfileProps) {
             <div className='column is-half is-offset-one-quarter'>
                 <div className='box'>
                     <div className='has-text-centered'>
-                        <Gravatar email={user.email} />
+                        {getAvatar(user)}
                     </div>
                     <h3 className='title is-3'>
                         Hello {user.displayName || user.email}!
