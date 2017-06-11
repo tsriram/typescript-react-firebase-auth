919ad9c85ebd5da45f5bcba4520cc8e7a1848940
Sriram Thiagarajan <sri.sjc@gmail.com>
2017-06-11 14:54:27 +0530

    basic profile with gravatar & logout

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,31 @@
 import * as React from 'react';
+import { User } from 'firebase';
+const Gravatar = require('react-gravatar');
 
-export default function Profile(props: React.Props<any>) {
+import { ExtendedRouteProps } from '../routes';
+import { logout } from '../util/auth';
+
+interface ProfileProps extends ExtendedRouteProps {
+    user?: User;
+}
+
+export default function Profile(props: ProfileProps) {
+    const { user } = props;
     return (
-        <div>Profile</div>
+        <div className='container'>
+            <div className='column is-half is-offset-one-quarter'>
+                <div className='box'>
+                    <div className='has-text-centered'>
+                        <Gravatar email={user.email} />
+                    </div>
+                    <h3 className='title is-3'>
+                        Hello {user.displayName || user.email}!
+                    </h3>
+                    <button className='button is-outlined' onClick={() => logout()}>
+                        Logout
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
